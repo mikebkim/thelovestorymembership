075e8b86aff47430883df0839901a5f343206451
mikebkim <mikebkim@gmail.com>
2018-10-31 18:30:06 -0700

    adding nav collapse on click

--- js/index.js
+++ js/index.js
@@ -1,10 +1,30 @@
 // Nav Targets
 
-$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $(this).attr('href') ).offset().top
-    }, 1000);
-    return false;
+$('a[href^="#"]').on('click', function (event) {
+
+    var target = $(this.getAttribute('href'));
+
+    if (target.length) {
+        event.preventDefault();
+        $('html, body').stop().animate({
+            scrollTop: target.offset().top
+        }, 1000);
+    }
+
+});
+
+// Scroll to Top
+$(window).scroll(function () {
+    if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+        $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    } else {
+        $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    }
+});
+$('#return-to-top').click(function () {      // When arrow is clicked
+    $('body,html').animate({
+        scrollTop: 0                       // Scroll to top of body
+    }, 500);
 });
 
 // Underline Nav
